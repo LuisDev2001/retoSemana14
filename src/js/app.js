4685d82ffadbc9cf05f8d612bed59539f88ffc42
Luis Quispe <62967265+lquispeAvatar@users.noreply.github.com>
2020-07-04 10:07:32 -0500

    Print Information of api fake

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ const app = new Vue({
   el: "#app",
   data: {
     title: "",
-    url: "",
+    url_video: "",
     description: "",
 
     openSectionAddVideo: false,
@@ -10,6 +10,46 @@ const app = new Vue({
   methods: {
     toggleOpenAddVideoSection() {
       this.openSectionAddVideo = !this.openSectionAddVideo;
+    },
+    printVideo(videos) {
+      let containerListVideo = document.getElementById("js-list-video");
+      for (const video of videos) {
+        containerListVideo.innerHTML += `
+          <article class="card-video">
+            <div class="card-video-img">
+              <div class="card-video-actions">
+                <div class="edit-btn">
+                  <i class="fas fa-pen"></i>
+                </div>
+                <div class="delete-btn">
+                  <i class="fas fa-times"></i>
+                </div>
+              </div>
+              <div class="play-icon">
+                <i class="fas fa-play"></i>
+              </div>
+              <img src="${video.url_video}" alt="img" />
+            </div>
+            <div class="card-video-information">
+              <h3 class="card-video-title">${video.title}</h3>
+              <span class="card-video-view">0 visualizaciones</span>
+              <p class="card-video-description">${video.description}</p>
+              <a href="#" class="card-video-detail">
+                Ver detalle
+              </a>
+            </div>
+          </article>
+        `;
+      }
+    },
+    getVideo() {
+      fetch("http://localhost:3000/videos")
+        .then((response) => {
+          return response.json();
+        })
+        .then((videos) => {
+          this.printVideo(videos);
+        });
     },
   },
 });
